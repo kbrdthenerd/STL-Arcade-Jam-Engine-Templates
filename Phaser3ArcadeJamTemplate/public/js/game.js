5f6a78f18e3eeebc3224ab656445f3751fd2df98
Katherine Brennan <katherinebrennan@Katherines-MacBook-Pro.local>
2018-09-26 22:27:55 -0500

    added second player and started fullscreen support

--- Phaser3ArcadeJamTemplate/public/js/game.js
+++ Phaser3ArcadeJamTemplate/public/js/game.js
@@ -19,42 +19,95 @@ var config = {
 var game = new Phaser.Game(config);
 var hasLogged = false
 
+window.addEventListener("click",function(event){
+  event.stopPropagation()
+  fullscreen()
+},false);
+
+
+
 function preload() {
   this.load.image('player', 'assets/sp_player_0.png');
 }
 
 function create() {
-  addPlayer(this)
+  var player1Info = { color: 0xff0000, name: 'player1' }
+  var player2Info = { color: 0x0000ff, name: 'player2' }
+
+  addPlayer(this, player1Info)
+  addPlayer(this, player2Info)
+
   this.cursors = this.input.keyboard.createCursorKeys();
+  this.wKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+  this.aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+  this.sKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+  this.dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
   this.physics.world.setBounds(0, 0, 750, 900, true, true, true, true);
-
-
 }
 
 function update() {
-  if (this.player1) {
-    if (this.cursors.left.isDown) {
-      this.player1.setVelocityX(-100)
-    } 
-     if (this.cursors.right.isDown) {
-      this.player1.setVelocityX(100)
-    }
-     if (this.cursors.up.isDown) {
-      this.player1.setVelocityY(-100)
-    }
-     if (this.cursors.down.isDown) {
-      this.player1.setVelocityY(100)
-    }
+  var player1Buttons = { name: 'player1',
+    up: this.cursors.up,
+    down: this.cursors.down,
+    left: this.cursors.left,
+    right: this.cursors.right
   }
 
+  var player2Buttons = { name: 'player2',
+    up: this.wKey,
+    down: this.sKey,
+    left: this.aKey,
+    right: this.dKey
+  }
+
+  movePlayer(this, player1Buttons)
+  movePlayer(this, player2Buttons)
 }
 
 function addPlayer(self, playerInfo) {
-  self.player1 = self.physics.add.image(375, 800, 'player').setOrigin(0.5, 0.5);
-  self.player1.setDrag(100);
-  self.player1.setAngularDrag(100);
-  self.player1.setMaxVelocity(200);
+  var name = playerInfo.name
 
-  self.player1.body.setCollideWorldBounds(true);
-  self.player1.body.onWorldBounds = true;
+  self[name] = self.physics.add.image(375, 800, 'player').setOrigin(0.5, 0.5);
+  self[name].setDrag(100);
+  self[name].setAngularDrag(100);
+  self[name].setMaxVelocity(200);
+  self[name].setTint(playerInfo.color)
+
+  self[name].body.setCollideWorldBounds(true);
+  self[name].body.onWorldBounds = true;
+}
+
+function movePlayer(self, options) {
+  var player = self[options.name]
+  if (player) {
+    if (options.left.isDown) {
+      player.setVelocityX(-100)
+    }
+     if (options.right.isDown) {
+      player.setVelocityX(100)
+    }
+     if (options.up.isDown) {
+      player.setVelocityY(-100)
+    }
+     if (options.down.isDown) {
+      player.setVelocityY(100)
+    }
+  }
+}
+
+function fullscreen() {
+  var elem = document.documentElement;
+  if(document.fullscreenEnabled || document.webkitFullscreenEnabled || document.mozFullScreenEnabled ||document.msFullscreenEnabled)
+    {
+        if (elem.requestFullscreen) {
+          elem.requestFullscreen();
+        } else if (elem.msRequestFullscreen) {
+          elem.msRequestFullscreen();
+        } else if (elem.mozRequestFullScreen) {
+          elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullscreen) {
+          elem.webkitRequestFullscreen();
+        }
+    }
 }
