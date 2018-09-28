262cf8cf4de848409602d713461a1ad73d22f679
Katherine Brennan <katherinebrennan@Katherines-MacBook-Pro.local>
2018-09-27 20:56:02 -0500

    better full screen and added buttons

--- Phaser3ArcadeJamTemplate/public/js/game.js
+++ Phaser3ArcadeJamTemplate/public/js/game.js
@@ -19,14 +19,6 @@ var config = {
 var game = new Phaser.Game(config);
 var hasLogged = false
 
-window.addEventListener("click",function(event){
-  event.stopPropagation()
-  fullscreen()
-  //game.resize(window.innerWidth, window.innerHeight);
-},false);
-
-
-
 function preload() {
   this.load.image('player', 'assets/sp_player_0.png');
 }
@@ -43,8 +35,17 @@ function create() {
   this.aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
   this.sKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
   this.dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+  this.esc = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
 
-  this.physics.world.setBounds(0, 0, 750, 900, true, true, true, true);
+  this.period = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.PERIOD);
+  this.forwardSlash = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FORWARD_SLASH);
+
+  this.backTick = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.BACKTICK);
+  this.oneKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
+
+  console.log(window.innerWidth - 50)
+  console.log(window.innerWidth)
+  this.physics.world.setBounds(50, 50, (window.innerWidth - 100), (window.innerHeight - 100), true, true, true, true);
 }
 
 function update() {
@@ -52,18 +53,28 @@ function update() {
     up: this.cursors.up,
     down: this.cursors.down,
     left: this.cursors.left,
-    right: this.cursors.right
+    right: this.cursors.right,
+    button1: this.period,
+    button2: this.forwardSlash,
+    color1: 0xff0000,
+    color2: 0xffff00
   }
 
   var player2Buttons = { name: 'player2',
     up: this.wKey,
     down: this.sKey,
     left: this.aKey,
-    right: this.dKey
+    right: this.dKey,
+    button1: this.backTick,
+    button2: this.oneKey,
+    color1: 0x0000ff,
+    color2: 0x00ffff
   }
 
-  movePlayer(this, player1Buttons)
-  movePlayer(this, player2Buttons)
+  movePlayer(this, player1Buttons, Phaser)
+  movePlayer(this, player2Buttons, Phaser)
+
+  handleEscapePress(Phaser, this.esc)
 }
 
 function addPlayer(self, playerInfo) {
@@ -79,36 +90,35 @@ function addPlayer(self, playerInfo) {
   self[name].body.onWorldBounds = true;
 }
 
-function movePlayer(self, options) {
-  var player = self[options.name]
-  if (player) {
-    if (options.left.isDown) {
-      player.setVelocityX(-100)
-    }
-     if (options.right.isDown) {
-      player.setVelocityX(100)
-    }
-     if (options.up.isDown) {
-      player.setVelocityY(-100)
-    }
-     if (options.down.isDown) {
-      player.setVelocityY(100)
-    }
-  }
-}
-
-function fullscreen() {
-  var elem = document.documentElement;
-  if(document.fullscreenEnabled || document.webkitFullscreenEnabled || document.mozFullScreenEnabled ||document.msFullscreenEnabled)
-    {
-        if (elem.requestFullscreen) {
-          elem.requestFullscreen();
-        } else if (elem.msRequestFullscreen) {
-          elem.msRequestFullscreen();
-        } else if (elem.mozRequestFullScreen) {
-          elem.mozRequestFullScreen();
-        } else if (elem.webkitRequestFullscreen) {
-          elem.webkitRequestFullscreen();
+function movePlayer(self, options, Phaser) {
+    var player = self[options.name]
+    if (player) {
+        if (options.left.isDown) {
+            player.setVelocityX(-200)
+        }
+         if (options.right.isDown) {
+             player.setVelocityX(200)
+        }
+         if (options.up.isDown) {
+             player.setVelocityY(-200)
+        }
+         if (options.down.isDown) {
+             player.setVelocityY(200)
+        }
+        if(Phaser.Input.Keyboard.JustDown(options.button1)) {
+            player.setTint(options.color1)
+        }
+        if(Phaser.Input.Keyboard.JustDown(options.button2)) {
+            player.setTint(options.color2)
         }
     }
+}
+
+function handleEscapePress(Phaser, esc) {
+    if (Phaser.Input.Keyboard.JustDown(esc))
+     {
+         var remote = require('electron').remote;
+         var window = remote.getCurrentWindow();
+         window.close();
+     }
 }
